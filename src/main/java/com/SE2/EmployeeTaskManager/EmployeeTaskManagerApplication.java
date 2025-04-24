cca04493364185ab1525a48cd6faab71c0d25cda
Ali Azzam <alihesham20@gmail.com>
2025-04-24 08:23:40 +0200

    project structure

---
+++ src/main/java/com/SE2/EmployeeTaskManager/EmployeeTaskManagerApplication.java
@@ -0,0 +1,14 @@
+package com.SE2.EmployeeTaskManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeTaskManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeTaskManagerApplication.class, args);
+		System.out.println("Employee Task Manager Application is running");
+	}
+
+}
